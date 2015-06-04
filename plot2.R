5133a37c9f39ce9f13926d62aad79a174aa420f0
chigandhi <chigandhi72@gmail.com>
2015-06-04 14:26:20 -0400

    plot 2 file

---
+++ plot2.R
@@ -0,0 +1,11 @@
+## plot2.R
+## Chirag Gandhi
+## Change this to the correct working directory
+workingDirectory <- c("C:\\Users\\Chirag\\chirag personal\\US\\coursera\\ExData")
+outputDirectory <- c("C:\\Users\\Chirag\\chirag personal\\US\\coursera\\ExData\\ExData_Plotting1")
+
+## DO NOT change below this
+## set working directory
+setwd(workingDirectory)
+
+## Read the column names
