bf5d0432fc4c1a945140ba83f9417c7e4a6b09b8
chigandhi <chigandhi72@gmail.com>
2015-06-05 08:19:16 -0400

    screen plot done

--- plot2.R
+++ plot2.R
@@ -12,14 +12,10 @@ setwd(workingDirectory)
 setClass('myDate')
 setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
 
-setClass ('myTime')
-setAs("character","myTime", function(from) as.POSIXlt(from, format="%H:%M:%S") )
-
-powerTable <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?", colClasses= c("myDate", "myTime", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-
-#powerTable$timestamp <- as.POSIXct(paste(powerTable$Date, powerTable$Time), "%d/%m/%Y %H:%M:%S")
+powerTable <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?", colClasses= c("myDate", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 subsetPower <- subset(powerTable,(powerTable$Date == as.Date("2007/02/01") | powerTable$Date == as.Date("2007/02/02")))
-#subsetPower <- rbind(subsetPower, powerTable[,(powerTable$Date == as.Date("2007/02/02"))])
 
-plot(x=subsetPower$Time, y=subsetPower$Global_active_power, type="l")
+subsetPower$timestamp <- as.POSIXct(paste(subsetPower$Date, subsetPower$Time), "%d/%m/%Y %H:%M:%S")
+
+plot(x=subsetPower$timestamp, y=subsetPower$Global_active_power, type="l", xlab=" ", ylab="Global Active Power (kilowatts)")
