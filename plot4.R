d449ca56b1711b0de128df2e99577b7c2e6d94aa
chigandhi <chigandhi72@gmail.com>
2015-06-05 08:57:59 -0400

    added plots

--- plot4.R
+++ plot4.R
@@ -17,3 +17,22 @@ powerTable <- read.table("household_power_consumption.txt", header=T, sep=";", n
 subsetPower <- subset(powerTable,(powerTable$Date == as.Date("2007/02/01") | powerTable$Date == as.Date("2007/02/02")))
 
 subsetPower$timestamp <- as.POSIXct(paste(subsetPower$Date, subsetPower$Time), "%d/%m/%Y %H:%M:%S")
+
+par (mfrow=c(2,2))
+
+## plot on top left
+plot(x=subsetPower$timestamp, y=subsetPower$Global_active_power, type="l", xlab=" ", ylab="Global Active Power (kilowatts)")
+
+## plot on top right
+plot(x=subsetPower$timestamp, y=subsetPower$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+## plot on bottom left
+colors <- c("black", "red", "blue")
+
+plot(x=subsetPower$timestamp, y=subsetPower$Sub_metering_1, type="l", xlab=" ", ylab="Energy sub metering", col="black")
+lines(x=subsetPower$timestamp, y=subsetPower$Sub_metering_2, col="red")
+lines(x=subsetPower$timestamp, y=subsetPower$Sub_metering_3, col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col= colors)
+
+## plot on bottom right
+plot(x=subsetPower$timestamp, y=subsetPower$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
